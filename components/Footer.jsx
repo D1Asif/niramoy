ae1b15c78442eb69d4821786d8dfb798936c7cf7
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-08-26 21:35:34 +0600

    footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 
 export default function Footer() {
     return (
         <footer className="py-6 md:py-4 mt-8">
             <div className="container mx-auto">
                 <p className="text-center text-md text-gray-600 dark:text-gray-400">
-                    Made with 💖 | By Bikolpo
+                    Made with 💖 | By Bikolpo | <Link className='underline' href={"https://discord.gg/rYM4g8HsAu"}>Join Discord</Link>
                 </p>
             </div>
         </footer>
