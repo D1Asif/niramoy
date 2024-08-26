fa047fc99992e3ad06bd2f8ebe5b94ab6a3f2ee2
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-08-26 19:08:54 +0600

    about us added

--- app/(home)/about-us/page.js
+++ app/(home)/about-us/page.js
@@ -1,9 +1,27 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function AboutUsPage() {
   return (
-    <div className='container'>
-        <div>Niramoy is a social activity platform to support patients</div>
+    <div className='container min-h-[75vh]'>
+      <div className='pt-10 flex flex-col gap-7 max-w-[900px] mx-auto'>
+        <h1 className='text-center text-2xl font-semibold py-3 '>Niramoy</h1>
+        <div className=' text-gray-400 text-xl '>
+          Niramoy is a social initiative to help the affected people in the student movement and the flood with medical support, funding and other possible means.
+        </div>
+        <div className=' text-gray-400 text-xl '>
+          It's the first project from Bikolpo, a social work collaboration community of energetic people across the country committed to work for a better Bangladesh.
+        </div>
+        <div className=' text-gray-400 text-xl '>
+          Join our <Link className='underline text-primary' href={"https://discord.gg/rYM4g8HsAu"}>Discord</Link> community to get involved and be a part of the initiative!
+        </div>
+        <div className='text-gray-400 text-xl'>
+          Website contributors: &nbsp; 
+          <Link className='underline' href={"https://www.facebook.com/leon.wasiul"}>Wasiul Haque</Link>
+          ,&nbsp; 
+          <Link className='underline' href={"https://www.facebook.com/dewan.asifurrahman/"}>Dewan Asifur Rahman</Link>
+        </div>
+      </div>
     </div>
   )
 }
