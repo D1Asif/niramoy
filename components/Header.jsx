a9d1608366ce5cfb06c7b33670723f0da13f6f0a
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-09-27 21:41:13 +0600

    number update

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,15 +11,15 @@ export default function Header() {
             <div className="flex flex-col md:flex-row justify-center p-4 space-y-4 md:space-y-0 md:space-x-4">
                 <div className="bg-red-100 text-red-600 rounded-2xl py-3 px-6 flex flex-col items-center">
                     <span className="font-semibold">Current Injured Patients</span>
-                    <span className="text-2xl font-bold">5000+</span>
+                    <span className="text-2xl font-bold">48+</span>
                 </div>
                 <div className="bg-yellow-100 text-orange-600 rounded-2xl py-3 px-6 flex flex-col items-center">
                     <span className="font-semibold">Critically Injured</span>
-                    <span className="text-2xl font-bold">3300+</span>
+                    <span className="text-2xl font-bold">43+</span>
                 </div>
                 <div className="bg-green-100 text-green-600 rounded-2xl py-3 px-6 flex flex-col items-center">
                     <span className="font-semibold">Fund Raised (BDT)</span>
-                    <span className="text-2xl font-bold">30,000</span>
+                    <span className="text-2xl font-bold">31,500</span>
                 </div>
             </div>
         </>
